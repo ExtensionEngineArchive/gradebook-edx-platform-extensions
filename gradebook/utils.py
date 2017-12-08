7ac94db51aaf9e43ecbd908b478864091fce858e
Ivan Ivic <ivivic1992@gmail.com>
2017-12-08 10:47:59 +0100

    Moved gradebook calculation methods from view to utils.
    We need the gradebook calculation in edx platform. It makes sense
    to move and create the calculation methods/functions in utils file
    so they can be used elsewhere without calling the class.
    
    NDPD-740

--- gradebook/utils.py
+++ gradebook/utils.py
@@ -3,15 +3,21 @@ Utils methods for gradebook app
 """
 import json
 
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 from django.db import transaction
 from django.utils.decorators import method_decorator
 
-from xmodule.modulestore import EdxJSONEncoder
-from xmodule.modulestore.django import modulestore
 from course_blocks.api import get_course_blocks
 from courseware import grades
-from courseware.courses import get_course
+from courseware.access import is_staff_or_instructor_on_course
+from courseware.courses import get_course, get_course_with_access
 from gradebook.models import StudentGradebook
+from instructor.offline_gradecalc import student_grades
+from instructor.views.gradebook_api import get_letter_grade, manual_grading_xblock_patcher
+from opaque_keys.edx.keys import CourseKey
+from xmodule.modulestore import EdxJSONEncoder
+from xmodule.modulestore.django import modulestore
 
 
 @method_decorator(transaction.non_atomic_requests)
@@ -59,3 +65,162 @@ def get_json_data(obj):
     except:
         json_data = {}
     return json_data
+
+
+def get_ordered_grades(course):
+    return sorted(course.grade_cutoffs.items(), key=lambda i: i[1], reverse=True)
+
+
+def get_course_sections(courseware_summary):
+    graded_sections = {}
+
+    for chapter in courseware_summary:
+        chapter_name = chapter['display_name']
+
+        for section in chapter['sections']:
+            if len(section['scores']) > 0 and section['graded']:
+                for score in section['scores']:
+                    graded_sections[score.module_id.block_id] = {
+                        'chapter_name': chapter_name,
+                        'possible_score': score.possible,
+                    }
+                graded_sections[section['url_name']] = {
+                    'chapter_name': chapter_name,
+                    'possible_score': section['section_total'].possible,
+                }
+
+    return graded_sections
+
+
+def get_enrolled_non_staff_students(course, course_key):
+    students = User.objects.filter(
+        courseenrollment__course_id=course_key,
+        courseenrollment__is_active=1
+    ).order_by('username').select_related('profile')
+
+    return [student for student in students if not is_staff_or_instructor_on_course(student, course)]
+
+
+def get_params_for_grade_summaries(user, course_id):
+    course_key = CourseKey.from_string(course_id)
+    course = get_course_with_access(user, 'staff', course_key, depth=None)
+    course_structure = get_course_blocks(user, course.location)
+    courseware_summary = grades.progress_summary(user, course, course_structure, extended_data=True)
+
+    return {
+        'course': course,
+        'course_structure': course_structure,
+        'graded_sections': get_course_sections(courseware_summary),
+        'non_staff_students': get_enrolled_non_staff_students(course, course_key),
+    }
+
+
+def get_grade_summaries(request, students, course, course_structure, graded_sections):
+    with modulestore().bulk_operations(course.location.course_key):
+        student_info = []
+        len_of_modules = 0
+        greatest_student_index = None
+
+        for index, student in enumerate(students):
+            course_id = course.id.to_deprecated_string()
+            gradebook_entry = {
+                'username': student.username,
+                'full_name': student.get_full_name(),
+                'user_id': student.id,
+                'email': student.email,
+                'course_id': course_id,
+                'progress_page_url': reverse(
+                    'student_progress',
+                    kwargs=dict(course_id=course_id, student_id=student.id)
+                ),
+                # NDPD-631, NDPD-641: Pass the graded_sections here, again via an optional parameter
+                'grade_summary': student_grades(
+                    student,
+                    request,
+                    course,
+                    course_structure=course_structure,
+                    graded_sections=graded_sections,
+                    keep_raw_scores=True
+                ),
+            }
+
+            if len_of_modules < gradebook_entry['grade_summary']['manual_graded_total_count']:
+                len_of_modules = gradebook_entry['grade_summary']['manual_graded_total_count']
+                greatest_student_index = index
+
+            student_info.append(gradebook_entry)
+
+        if greatest_student_index > 0 and len_of_modules > 0:
+            student_info.insert(0, student_info.pop(greatest_student_index))
+
+        student_info = manual_grading_xblock_patcher(student_info, greatest_student_index, len_of_modules)
+    return student_info
+
+
+def get_updated_grade_summaries(request, students, course, course_structure, graded_sections):
+    grade_summaries = get_grade_summaries(
+        request,
+        students,
+        course,
+        course_structure,
+        graded_sections,
+    )
+    ordered_grades = get_ordered_grades(course)
+    update_grade_summaries(grade_summaries, ordered_grades)
+    return grade_summaries
+
+
+def update_grade_summaries(grade_summaries, ordered_grades):
+    for student_summary in grade_summaries:
+        grade_summary = student_summary['grade_summary']
+        grade_summary.update({
+            'current_letter_grade': get_letter_grade(grade_summary['current_percent'], ordered_grades),
+            'total_letter_grade': get_letter_grade(grade_summary['percent'], ordered_grades),
+        })
+
+        for section in grade_summary['section_breakdown']:
+            letter_grade = None
+            percent = section['percent']
+
+            if type(percent) is unicode:
+                displayed_value = percent
+            else:
+                displayed_value = section.get('displayed_value') or '{0:.2f}%'.format(100 * percent)
+
+                if type(percent) is float and percent > 0:
+                    letter_grade = get_letter_grade(percent, ordered_grades)
+
+            # TODO: Move to grades.py
+            if '=' in section['detail']:
+                # Examples:
+                # Weekly Problems Average = 0%
+                # Exam 2 = 0%
+                # Represents a score of an entire graded category (e.g. Weekly Problems or Exam 2)
+                scores = grade_summary['totaled_scores'].get(section['category'], [])
+                score_earned = 0
+                score_possible = 0
+                for score in scores:
+                    score_earned += score.earned
+                    score_possible += score.possible
+            elif '-' in section['detail']:
+                # Example: Weekly Homework 14 Unreleased - 0% (?/?)
+                # Represents a score for a graded assessment within a section
+                score_from_label = section['detail'].split(' ')[-1]
+                score_earned = score_from_label.split('(')[1].split('/')[0]
+                score_possible = score_from_label.split(')')[0].split('/')[1]
+            else:
+                score_earned = section.get('score_earned', 0)
+                score_possible = section.get('score_possible', 0)
+
+            try:
+                grade_description = '({:0.2f}/{:0.2f})'.format(float(score_earned), float(score_possible))
+            except:
+                grade_description = '({}/{})'.format(score_earned, score_possible)
+
+            section.update({
+                'displayed_value': displayed_value,
+                'grade_description': grade_description,
+                'letter_grade': letter_grade,
+                'score_earned': score_earned,
+                'score_possible': score_possible,
+            })
--- gradebook/views.py
+++ gradebook/views.py
@@ -5,41 +5,32 @@ from datetime import timedelta
 
 from django.conf import settings
 from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
 from django.db import transaction
 from django.db.models import Count, F, Max, Min
 from django.utils.translation import ugettext_lazy as _
 
-from opaque_keys.edx.keys import CourseKey
-from rest_framework import generics, status
-from rest_framework.response import Response
-
 from course_blocks.api import get_course_blocks
-from courseware import grades
-from courseware.access import is_staff_or_instructor_on_course
-from courseware.courses import get_course_with_access
 from courseware.models import StudentModule
-from instructor.offline_gradecalc import student_grades
-from instructor.views.api import require_level
-from instructor.views.gradebook_api import get_letter_grade, manual_grading_xblock_patcher
-from progress.models import StudentProgress, CourseModuleCompletion
-from student.models import CourseEnrollment
-from student.roles import get_aggregate_exclusion_user_ids
-from xmodule.modulestore.django import modulestore
-
 from gradebook.api_utils import (
-    generate_base_uri,
-    get_time_series_data,
-    parse_datetime,
-    get_ids_from_list_param,
     css_param_to_list,
+    generate_base_uri,
+    get_ids_from_list_param,
+    get_time_series_data,
+    parse_datetime
 )
-from gradebook.courseware_access import get_course, get_course_key, course_exists
+from gradebook.courseware_access import course_exists, get_course, get_course_key
 from gradebook.models import StudentGradebook
 from gradebook.pagination import GradebookPagination
 from gradebook.permissions import SecureAPIView, SecureListAPIView
 from gradebook.serializers import CourseLeadersSerializer, GradeSerializer, StudentGradebookEntrySerializer
-
+from gradebook.utils import get_params_for_grade_summaries, get_updated_grade_summaries
+from instructor.views.api import require_level
+from progress.models import CourseModuleCompletion, StudentProgress
+from rest_framework import generics, status
+from rest_framework.response import Response
+from student.models import CourseEnrollment
+from student.roles import get_aggregate_exclusion_user_ids
+from xmodule.modulestore.django import modulestore
 
 log = logging.getLogger(__name__)
 
@@ -390,148 +381,18 @@ class CourseGradeBook(generics.ListAPIView):
     def dispatch(self, request, *args, **kwargs):
         return super(CourseGradeBook, self).dispatch(request, *args, **kwargs)
 
-    def get_ordered_grades(self, course):
-        return sorted(course.grade_cutoffs.items(), key=lambda i: i[1], reverse=True)
-
-    def get_course_sections(self, courseware_summary):
-        graded_sections = {}
-
-        for chapter in courseware_summary:
-            chapter_name = chapter['display_name']
-
-            for section in chapter['sections']:
-                if len(section['scores']) > 0 and section['graded']:
-                    for score in section['scores']:
-                        graded_sections[score.module_id.block_id] = {
-                            'chapter_name': chapter_name,
-                            'possible_score': score.possible,
-                        }
-                    graded_sections[section['url_name']] = {
-                        'chapter_name': chapter_name,
-                        'possible_score': section['section_total'].possible,
-                    }
-
-        return graded_sections
-
-    def get_enrolled_non_staff_students(self, course, course_key):
-        students = User.objects.filter(
-            courseenrollment__course_id=course_key,
-            courseenrollment__is_active=1
-        ).order_by('username').select_related('profile')
-
-        return [student for student in students if not is_staff_or_instructor_on_course(student, course)]
-
-    def get_grade_summaries(self, request, students, course, course_structure, graded_sections):
-        with modulestore().bulk_operations(course.location.course_key):
-            student_info = []
-            len_of_modules = 0
-            greatest_student_index = None
-
-            for index, student in enumerate(students):
-                course_id = course.id.to_deprecated_string()
-                gradebook_entry = {
-                    'username': student.username,
-                    'full_name': student.get_full_name(),
-                    'user_id': student.id,
-                    'email': student.email,
-                    'course_id': course_id,
-                    'progress_page_url': reverse(
-                        'student_progress', 
-                        kwargs=dict(course_id=course_id, student_id=student.id)
-                    ),
-                    # NDPD-631, NDPD-641: Pass the graded_sections here, again via an optional parameter
-                    'grade_summary': student_grades(
-                        student, 
-                        request, 
-                        course, 
-                        course_structure=course_structure, 
-                        graded_sections=graded_sections, 
-                        keep_raw_scores=True
-                    ),
-                }
-
-                if len_of_modules < gradebook_entry['grade_summary']['manual_graded_total_count']:
-                    len_of_modules = gradebook_entry['grade_summary']['manual_graded_total_count']
-                    greatest_student_index = index
-
-                student_info.append(gradebook_entry)
-
-            if greatest_student_index > 0 and len_of_modules > 0:
-                student_info.insert(0, student_info.pop(greatest_student_index))
-
-            student_info = manual_grading_xblock_patcher(student_info, greatest_student_index, len_of_modules)
-        return student_info
-
-    def update_grade_summaries(self, grade_summaries, ordered_grades):
-        for student_summary in grade_summaries:
-            grade_summary = student_summary['grade_summary']
-            grade_summary.update({
-                'current_letter_grade': get_letter_grade(grade_summary['current_percent'], ordered_grades),
-                'total_letter_grade': get_letter_grade(grade_summary['percent'], ordered_grades),
-            })
-
-            for section in grade_summary['section_breakdown']:
-                letter_grade = None
-                percent = section['percent']
-
-                if type(percent) is unicode:
-                    displayed_value = percent
-                else:
-                    displayed_value = section.get('displayed_value') or '{0:.2f}%'.format(100 * percent)
-
-                    if type(percent) is float and percent > 0:
-                        letter_grade = get_letter_grade(percent, ordered_grades)
-
-                # TODO: Move to grades.py
-                if '=' in section['detail']:
-                    # Examples:
-                    # Weekly Problems Average = 0%
-                    # Exam 2 = 0%
-                    # Represents a score of an entire graded category (e.g. Weekly Problems or Exam 2)
-                    scores = grade_summary['totaled_scores'].get(section['category'], [])
-                    score_earned = 0
-                    score_possible = 0
-                    for score in scores:
-                        score_earned += score.earned
-                        score_possible += score.possible
-                elif '-' in section['detail']:
-                    # Example: Weekly Homework 14 Unreleased - 0% (?/?)
-                    # Represents a score for a graded assessment within a section
-                    score_from_label = section['detail'].split(' ')[-1]
-                    score_earned = score_from_label.split('(')[1].split('/')[0]
-                    score_possible = score_from_label.split(')')[0].split('/')[1]
-                else:
-                    score_earned = section.get('score_earned', 0)
-                    score_possible = section.get('score_possible', 0)
-
-                try:
-                    grade_description = '({:0.2f}/{:0.2f})'.format(float(score_earned), float(score_possible))
-                except:
-                    grade_description = '({}/{})'.format(score_earned, score_possible)
-
-                section.update({
-                    'displayed_value': displayed_value,
-                    'grade_description': grade_description,
-                    'letter_grade': letter_grade,
-                    'score_earned': score_earned,
-                    'score_possible': score_possible,
-                })
-
     def list(self, request, course_id):
         paginator = self.pagination_class()
 
-        course_key = CourseKey.from_string(course_id)
-        course = get_course_with_access(request.user, 'staff', course_key, depth=None)
-        ordered_grades = self.get_ordered_grades(course)
-        course_structure = get_course_blocks(request.user, course.location)
-        courseware_summary = grades.progress_summary(request.user, course, course_structure, extended_data=True)
-        graded_sections = self.get_course_sections(courseware_summary)
-        non_staff_students = self.get_enrolled_non_staff_students(course, course_key)
-
-        page = paginator.paginate_queryset(non_staff_students, request)
-
-        grade_summaries = self.get_grade_summaries(request, page, course, course_structure, graded_sections)
-        self.update_grade_summaries(grade_summaries, ordered_grades)
+        grade_summaries_params = get_params_for_grade_summaries(request.user, course_id)
+        page = paginator.paginate_queryset(grade_summaries_params['non_staff_students'], request)
+        grade_summaries = get_updated_grade_summaries(
+            request,
+            page,
+            grade_summaries_params['course'],
+            grade_summaries_params['course_structure'],
+            grade_summaries_params['graded_sections'],
+        )
 
         serializer = StudentGradebookEntrySerializer(grade_summaries, many=True)
 
